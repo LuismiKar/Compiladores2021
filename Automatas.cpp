b49549b63bcdc36d67c314ff35729ba4b934c89a
LuismiKar <77997810+LuismiKar@users.noreply.github.com>
2021-08-16 17:50:55 -0500

    Add files via upload

---
+++ Automatas.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#define i 0
+using namespace std;
+
+
+class Afd{
+	int i;
+	int estados[i];
+	String cadena;
+	bool resultado;
+	
+	public:
+		Afd(int tam, String cad){
+			i = tam;
+			cadena = cad;
+		}
+	
+};
+
+class Afnd{
+	int i;
+	int estados[i];
+	String cadena;
+	bool resultado;
+	
+	public:
+		Afd(int tam, String cad){
+			i = tam;
+			cadena = cad;
+		}
+	
+};
+
+
